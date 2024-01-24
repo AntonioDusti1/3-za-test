f96b27a406822c040137690d7f4bc298f31fe1be
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-24 16:33:34 +0100

    Add files via upload

---
+++ brojslova.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace red
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši riječ:");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> brojslova = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (brojslova.ContainsKey(c))
+                {
+                    brojslova[c]++;
+                }
+                else
+                {
+                    brojslova[c] = 1;
+                }
+            }
+
+            char najviseslova = brojslova.OrderByDescending(x => x.Value).First().Key;
+            int povezanost = brojslova[najviseslova];
+
+            Console.WriteLine($"Najcesce slov u riječi je  '{najviseslova}' ima ga oko {povezanost}.");
+            Console.ReadLine();
+        }
+    }
+}
