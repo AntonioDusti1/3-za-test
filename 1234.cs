056abacf231d30c6457f64a9aaa4feb7cd44d8ce
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-24 16:46:32 +0100

    Add files via upload

---
+++ 1234.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1234
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisi pozitivan broj:");
+            int broj = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < broj; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
