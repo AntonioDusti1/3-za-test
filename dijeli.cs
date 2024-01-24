2bce5927bfc3e33c007b1addc51b99c37c74675a
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-24 16:44:02 +0100

    Add files via upload

---
+++ dijeli.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace novo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši prvi broj:");
+            int broj1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Upiši drugi broj:");
+            int broj2 = int.Parse(Console.ReadLine());
+
+            int najvećidijeljivibroj = Math.Min(broj1, broj2);
+
+            while (broj1 % najvećidijeljivibroj != 0 || broj2 % najvećidijeljivibroj != 0)
+            {
+               najvećidijeljivibroj--;
+            }
+
+            Console.WriteLine($"Najveći broj dijeljiv {broj1} i {broj2} je {najvećidijeljivibroj}.");
+            Console.ReadLine();
+        }
+    }
+}
